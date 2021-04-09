8672c6cb9bb0dab28aea13e59980c4b280010ab9
chaminjae <beingeskimo@gmail.com>
2021-04-09 10:25:16 +0900

    UPDATE : gitignore

--- toc/client/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = function (app) {
-  app.use(
-    createProxyMiddleware("/api", {
-      target: "http://211.105.65.18:1234/",
-      changeOrigin: true,
-    })
-  );
-};
--- toc/config/salt.js
+++
@@ -1,3 +0,0 @@
-const salt = "hellonicetomeetyouhowareyou";
-
-module.exports = salt;
